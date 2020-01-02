c47ccdc3cb2dd898969d0adb1d31845d7e4dc456
Taylor Young <taylorjayoung@gmail.com>
2020-01-02 12:19:48 -0500

    footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  get 'privacy', to: 'static_pages#privacy'
+
   resource :dashboard, only: [:show]
 
   resources :courses, only: [:index, :show]
